62f085e836e46bcdde8c7c3602f828061b2082fb
wliederer <williamliederer@gmail.com>
2019-11-18 15:44:25 -0500

    needs angular

--- groceryservice/src/main/java/com/groceryservice/controller/ItemController.java
+++ groceryservice/src/main/java/com/groceryservice/controller/ItemController.java
@@ -24,7 +24,5 @@ public class ItemController {
 			gr.save(item);	
 	}
 	
-	
-	
 
 }
--- groceryservice/src/main/java/com/groceryservice/model/Item.java
+++ groceryservice/src/main/java/com/groceryservice/model/Item.java
@@ -1,6 +1,6 @@
 package com.groceryservice.model;
 
-import javax.persistence.CascadeType;
+import javax.persistence.CascadeType; 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
